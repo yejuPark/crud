9ad7b5b0bcc84b1c4c7d097f3f7a41ad2205deb7
yejuPark <pyj8977@daum.net>
2023-08-22 11:21:00 +0900

    Read(All)

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'index.html', context)

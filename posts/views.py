b9b6f0675851d31b0ac0764f1b089a42d47ca8a9
yejuPark <pyj8977@daum.net>
2023-08-22 11:44:01 +0900

    Read(1)

--- posts/views.py
+++ posts/views.py
@@ -10,3 +10,12 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+
+def detail(request, id):
+    post = Post.objects.get(id=id)
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'detail.html', context)
